67d3b8824030cbdd51b893485274f2bbfb2c877e
Mark Edward Vogt <mark_vogt@hotmail.com>
2017-02-26 22:40:35 -0600

    Add files via upload

---
+++ MarksWFATest01/MarksWFATest01/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace MarksWFATest01
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGreetings = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGreetings
+            // 
+            this.lblGreetings.AutoSize = true;
+            this.lblGreetings.Location = new System.Drawing.Point(30, 34);
+            this.lblGreetings.Name = "lblGreetings";
+            this.lblGreetings.Size = new System.Drawing.Size(135, 13);
+            this.lblGreetings.TabIndex = 0;
+            this.lblGreetings.Text = "Hello World Mark  & Michael";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblGreetings);
+            this.Name = "frmMain";
+            this.Text = "frmMain - Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGreetings;
+    }
+}
+
